b163d87023c997b0f70ffd8e6bd324a4bff5b49c
Daniel Cender <danielcender@gmail.com>
2024-01-18 20:14:30 -0600

    updates

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,17 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-
     println!("Pattern {:?}, path: {:?}", args.pattern,args.path);
     
-    // let content = std::fs::read_to_string(&args.path).expect("File not found at that path");
-
     let file = std::fs::File::open(&args.path).expect("File not found at that path");
-
-
     let mut buf_reader = BufReader::new(&file);
     let mut line = String::new();
 
     loop {
         let line_size = buf_reader.read_line(&mut line).expect("Error reading line");
-
         if line_size == 0 {
             break;
-        } 
+        }
 
         if line.contains(&args.pattern) {
             println!("{:?} -> Bytes: {:?}", line, line_size);
